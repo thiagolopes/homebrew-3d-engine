e943b7bdac3ae0dd61bceb96c5ac14d8c2e0167b
thiagolopes <thiagolopes@protonmail.com>
2023-07-13 15:31:55 -0300

    Implement textures

---
+++ src/textures.hh
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+
+class Texture {
+private:
+  unsigned int t_render_id;
+  std::string t_filepath;
+  unsigned char *t_localbuffer;
+  int t_width, t_height, t_bpp;
+
+public:
+  Texture(const std::string &file);
+  ~Texture();
+
+  void bind(unsigned int slot = 0) const;
+  void unbind() const;
+
+  inline int get_width() const {
+    return t_width;
+  };
+  inline int get_height() const {
+    return t_height;
+  };
+};
