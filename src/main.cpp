a8a7cec0a8465568419b6aad97f68b020df45ff2
thiagolopes <thiagolopes@protonmail.com>
2023-08-10 16:55:17 -0300

    Rename var cube_word_positions -> word_positions

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ void mouse_callback(GLFWwindow *window, double xposIn, double yposIn);
 void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
 // triagle positions and incidices - vertex collection
 
-glm::vec3 cube_word_positions[] = {glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
+glm::vec3 word_positions[] = {glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
                                    glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
                                    glm::vec3(2.4f, -0.4f, -3.5f),  glm::vec3(-1.7f, 3.0f, -7.5f),
                                    glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f),
@@ -144,7 +144,7 @@ int main(void) {
                             0.1f, 100.0f);
 
     shader.bind();
-    model = glm::translate(glm::mat4(1.0f), cube_word_positions[0]);  // multiply z to use positive bar in gui
+    model = glm::translate(glm::mat4(1.0f), word_positions[0]);  // multiply z to use positive bar in gui
     model = glm::scale(model, glm::vec3(1.0f));
 
     shader.set_uniform_vec3("u_DirLight.direction", directional_light.direction);
