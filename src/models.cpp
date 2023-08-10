9fc7017590bc5826a2599607bc223d8992ff8fb9
thiagolopes <thiagolopes@protonmail.com>
2023-08-10 18:49:21 -0300

    Remove mesh iteration

--- src/models.cpp
+++ src/models.cpp
@@ -1,6 +1,7 @@
 #include "models.hh"
 #include "vendor/obj_loader/obj_loader.h"
 
+// CAUTION: This implemention blend all the meshes of file in one;
 Model::Model(const std::string &model_name) {
   objl::Loader loader;
   std::string path_models = "res/models/";
@@ -14,22 +15,20 @@ Model::Model(const std::string &model_name) {
   std::vector<Vertex> vertices;
   std::vector<unsigned int> indices;
 
-  for (size_t m = 0; m < loader.LoadedMeshes.size(); m++) {
-    for (size_t i = 0; i < loader.LoadedMeshes[m].Vertices.size(); i++) {
-      Vertex v;
-      v.position.x = loader.LoadedMeshes[m].Vertices[i].Position.X;
-      v.position.y = loader.LoadedMeshes[m].Vertices[i].Position.Y;
-      v.position.z = loader.LoadedMeshes[m].Vertices[i].Position.Z;
+  for (size_t i = 0; i < loader.LoadedVertices.size(); i++) {
+    Vertex v;
+    v.position.x = loader.LoadedVertices[i].Position.X;
+    v.position.y = loader.LoadedVertices[i].Position.Y;
+    v.position.z = loader.LoadedVertices[i].Position.Z;
 
-      v.normal.x = loader.LoadedMeshes[m].Vertices[i].Normal.X;
-      v.normal.y = loader.LoadedMeshes[m].Vertices[i].Normal.Y;
-      v.normal.z = loader.LoadedMeshes[m].Vertices[i].Normal.Z;
+    v.normal.x = loader.LoadedVertices[i].Normal.X;
+    v.normal.y = loader.LoadedVertices[i].Normal.Y;
+    v.normal.z = loader.LoadedVertices[i].Normal.Z;
 
-      v.tex_coord.x = loader.LoadedMeshes[m].Vertices[i].TextureCoordinate.X;
-      v.tex_coord.y = loader.LoadedMeshes[m].Vertices[i].TextureCoordinate.Y;
+    v.tex_coord.x = loader.LoadedVertices[i].TextureCoordinate.X;
+    v.tex_coord.y = loader.LoadedVertices[i].TextureCoordinate.Y;
 
-      vertices.push_back(v);
-    }
+    vertices.push_back(v);
   }
   for (size_t i = 0; i < loader.LoadedIndices.size(); i++) {
     indices.push_back(loader.LoadedIndices[i]);
