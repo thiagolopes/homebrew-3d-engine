50de9bde84bc8c4617060b3e57961e13366c682d
thiagolopes <thiagolopes@protonmail.com>
2023-08-11 17:01:35 -0300

    Create texture names vars

--- src/models.cpp
+++ src/models.cpp
@@ -6,9 +6,9 @@ Model::Model(const std::string &model_name) {
   objl::Loader loader;
   std::string path_models = "res/models/";
   std::string obj_name = model_name;
-  std::string obj_path = path_models + obj_name;
+  std::string obj_path = path_models + obj_name + "/";
 
-  if (!loader.LoadFile(obj_path + "/" + obj_name + ".obj")) {
+  if (!loader.LoadFile(obj_path + obj_name + ".obj")) {
     std::cout << "[ERROR] Fail to load model: " << obj_name << std::endl;
   }
 
@@ -33,11 +33,14 @@ Model::Model(const std::string &model_name) {
   for (size_t i = 0; i < loader.LoadedIndices.size(); i++) {
     indices.push_back(loader.LoadedIndices[i]);
   }
-  mesh = (new Mesh(vertices, indices));
+  mesh = new Mesh(vertices, indices);
 
-  material =
-      new Material(obj_path + "/" + loader.LoadedMaterials[0].map_Kd, obj_path + "/" + loader.LoadedMaterials[0].map_Ks,
-                   obj_path + "/" + loader.LoadedMaterials[0].map_Ke, loader.LoadedMaterials[0].Ns, 1.0f, false);
+  std::string diffuse_path = obj_path + loader.LoadedMaterials[0].map_Kd;
+  std::string specular_path = obj_path + loader.LoadedMaterials[0].map_Ks;
+  std::string emission_path = obj_path + obj_path + loader.LoadedMaterials[0].map_Ke;
+  float specular_expoent = loader.LoadedMaterials[0].Ns;
+
+  material = new Material(diffuse_path, specular_path, emission_path, specular_expoent, 1.0, false);
 }
 
 Model::~Model() {
