16b8e328f26926cf3b2e9b2089e6e0ee29bc12e7
thiagolopes <thiagolopes@protonmail.com>
2023-12-15 16:08:05 -0300

    Clear color background with color

--- src/window.cpp
+++ src/window.cpp
@@ -1,77 +1,73 @@
 #include "window.hh"
 
+#include <iostream>
+
 #include "inputs.hh"
-#include "mesh.hh"
 
 Window::Window(char *window_name, float width, float height)
-  : m_window(nullptr), m_width(width), m_height(height), m_deltatime(0), m_lastframe(0), _mouse(Mouse::get_instance()), _opengl(OpenGLCallback::get_instance())
-{
-  /* Initialize the library */
-  if (!glfwInit())
-    {
-      std::cerr << "[ERROR] In load GLFW!" << std::endl;
-      std::exit(EXIT_FAILURE);
-    }
+    : m_window(nullptr), m_width(width), m_height(height), m_deltatime(0), m_lastframe(0),
+      _mouse(Mouse::get_instance()), _opengl(OpenGLCallback::get_instance()) {
+    /* Initialize the library */
+    if (!glfwInit())
+        {
+            std::cerr << "[ERROR] In load GLFW!" << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
 
-  // set opengl to 3 and use CORE. this will make VAO especification mandatory
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // set opengl to 3 and use CORE. this will make VAO especification mandatory
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  /* Create a windowed mode window and its OpenGL context */
-  m_window = glfwCreateWindow(m_width, m_height, window_name, nullptr, nullptr);
-  if (!m_window)
-    {
-      std::cerr << "[ERROR] Fail in create window GLFW!" << std::endl;
-      glfwTerminate();
-      std::exit(EXIT_FAILURE);
-    }
+    /* Create a windowed mode window and its OpenGL context */
+    m_window = glfwCreateWindow(m_width, m_height, window_name, nullptr, nullptr);
+    if (!m_window)
+        {
+            std::cerr << "[ERROR] Fail in create window GLFW!" << std::endl;
+            glfwTerminate();
+            std::exit(EXIT_FAILURE);
+        }
 
-  /* Make the window's context current */
-  glfwMakeContextCurrent(m_window);
-  // init glew
-  if (glewInit() != GLEW_OK)
-    {
-      std::cout << "[ERROR] Fail in load graphical drive" << std::endl;
-      glfwTerminate();
-      std::exit(EXIT_FAILURE);
-    }
+    /* Make the window's context current */
+    glfwMakeContextCurrent(m_window);
+    // init glew
+    if (glewInit() != GLEW_OK)
+        {
+            std::cout << "[ERROR] Fail in load graphical drive" << std::endl;
+            glfwTerminate();
+            std::exit(EXIT_FAILURE);
+        }
 
-  // show opengl version
-  std::cout << "[LOG]" << glGetString(GL_VERSION) << std::endl;
-  update_deltatime_frame();
+    // show opengl version
+    std::cout << "[LOG]" << glGetString(GL_VERSION) << std::endl;
+    update_deltatime_frame();
 
-  // setup minors options by default
-  _opengl.set_window(get_window());
+    // setup minors options by default
+    _opengl.set_window(get_window());
 };
 
-Window::~Window() { glfwTerminate(); };
+Window::~Window() {
+    glfwTerminate();
+};
 
-float
-Window::get_time()
-{
-  return glfwGetTime();
+float Window::get_time() {
+    return glfwGetTime();
 }
 
-void
-Window::end_frame()
-{
-  _mouse.next_frame();
+void Window::end_frame() {
+    _mouse.next_frame();
 
-  update_deltatime_frame();
-  glfwSwapBuffers(m_window);
-  glfwPollEvents();
+    update_deltatime_frame();
+    glfwSwapBuffers(m_window);
+    glfwPollEvents();
 };
 
 // set a running
-bool
-Window::running()
-{
-  return !glfwWindowShouldClose(m_window);
+bool Window::running() {
+    return !glfwWindowShouldClose(m_window);
 };
 
-void
-Window::begin_frame() const
-{
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void Window::begin_frame() const {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 };
